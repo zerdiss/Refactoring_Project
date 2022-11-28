aa0184d983e03e1ca4cd1d83621c8f966fcbc9d9
juyoung kim <juyoungkim87@gmail.com>
2022-11-28 10:47:15 +0900

    DB에 저장되기 위해 Serializable로 변경

--- src/main/java/video/rental/demo/domain/Price.java
+++ src/main/java/video/rental/demo/domain/Price.java
@@ -1,23 +1,17 @@
 package video.rental.demo.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import java.io.Serializable;
 
-@Entity
-@Table(name = "PRICE_CODE", uniqueConstraints = {@UniqueConstraint(columnNames = {"_priceCode"})})
-public abstract class Price {
-	@Id
-	private int _priceCode;
+public abstract class Price implements Serializable {
+    private int _priceCode;
 
-	public Price(int price) {
-		_priceCode = price;
-	}
-	
-	public int priceCode() {
-		return _priceCode;
-	}
-	
-	abstract double getCharge(int dayRented);
+    public Price(int price) {
+        _priceCode = price;
+    }
+
+    public int priceCode() {
+        return _priceCode;
+    }
+
+    abstract double getCharge(int dayRented);
 }
--- src/main/java/video/rental/demo/domain/Video.java
+++ src/main/java/video/rental/demo/domain/Video.java
@@ -11,14 +11,13 @@ public class Video {
     @Id
     private String title;
     private Rating videoRating;
-    @OneToOne
+
     private Price priceCode;
 
     public static final int REGULAR = 1;
     public static final int NEW_RELEASE = 2;
     public static final int CHILDREN = 3;
 
-    @OneToOne
     private VideoType videoType;
     public static final int VHS = 1;
     public static final int CD = 2;
